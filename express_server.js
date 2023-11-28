125e81bd4e5ebeacb1aa3bea9d90d5b1a61d0a63
Eminimo <eminimoeghosa@gmail.com>
2023-11-28 00:05:03 -0500

    Add EJS dependency and set it as the view engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,6 +12,10 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
