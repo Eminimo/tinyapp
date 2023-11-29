2ad7060cd91ddb57c5d291d6a42511197244aa10
Eminimo <eminimoeghosa@gmail.com>
2023-11-28 23:19:01 -0500

    update

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Import the necessary modules
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -5,10 +6,25 @@ const PORT = 8080;
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 
+// Function to generate a random alphanumeric string
+function generateRandomString(length = 6) {
+  const alphanumericChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+    randomString += alphanumericChars.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Routes
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +32,34 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  // Assuming you have a form submission with a 'longURL' field in the request body
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+
+  // Add the new URL to the database
+  urlDatabase[shortURL] = longURL;
+
+  // Redirect to the URLs index page or do whatever is appropriate
+  res.redirect("/urls");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
