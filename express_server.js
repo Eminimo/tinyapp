f48e5232aba2024b31ff4b3952c9d8de51f66062
Eminimo <eminimoeghosa@gmail.com>
2023-11-29 22:11:15 -0500

    update

--- express_server.js
+++ express_server.js
@@ -1,67 +1,65 @@
-// Import the necessary modules
+// Import necessary modules
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+// Create Express app
 const app = express();
 const PORT = 8080;
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 
-// Function to generate a random alphanumeric string
-function generateRandomString(length = 6) {
-  const alphanumericChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let randomString = '';
+// Middleware to parse request bodies and cookies
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
-    randomString += alphanumericChars.charAt(randomIndex);
-  }
-
-  return randomString;
-}
-
+// Dummy URL database for testing
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// Routes
+// Function to generate a random string (for short URLs)
+const generateRandomString = () => {
+  // Implementation of generateRandomString function
+};
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// Dummy username for testing
+const dummyUsername = "TestUser";
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+// Modify existing routes to render templates properly
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies.username || dummyUsername,
+    urls: urlDatabase
+    // ... any other vars
+  };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  // Assuming you have a form submission with a 'longURL' field in the request body
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString();
-
-  // Add the new URL to the database
-  urlDatabase[shortURL] = longURL;
-
-  // Redirect to the URLs index page or do whatever is appropriate
+// POST route to remove a URL resource
+app.post("/urls/:id/delete", (req, res) => {
+  const urlId = req.params.id;
+  // Use the delete operator to remove the URL
+  delete urlDatabase[urlId];
+  // Redirect back to the urls_index page
   res.redirect("/urls");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id, longURL };
-  res.render("urls_show", templateVars);
+// ... (other routes)
+
+// POST route for handling login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  // Set the cookie named 'username' with the value from the request body
+  res.cookie("username", username);
+
+  // Redirect back to the /urls page
+  res.redirect("/urls");
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
